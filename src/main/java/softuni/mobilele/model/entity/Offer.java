b6dfc1429de4e76b362e65a441174d4d6ee62403
Lubo <wulu@abv.bg>
2022-06-02 21:48:41 +0300

    created entities

--- src/main/java/softuni/mobilele/model/entity/Offer.java
+++ src/main/java/softuni/mobilele/model/entity/Offer.java
@@ -7,4 +7,25 @@ import javax.persistence.Table;
 @Table(name = "offers")
 public class Offer extends BaseEntity{
     //TODO fields...
+    //· description – some text.
+    //
+    //· engine – enumerated value (GASOLINE, DIESEL, ELECTRIC, HYBRID).
+    //
+    //· imageUrl – the url of image.
+    //
+    //· mileage – a number.
+    //
+    //· price – the price of the offer.
+    //
+    //· transmission – enumerated value (MANUAL, AUTOMATIC).
+    //
+    //· year – the year of offered car.
+    //
+    //· created – a date and time.
+    //
+    //· modified – a date and time.
+    //
+    //· model – the model of a car.
+    //
+    //· seller – a user that sells the car.
 }
