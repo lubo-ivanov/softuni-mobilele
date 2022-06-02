b0b794c3dcd63362176628204a3b3f367b155863
Lubo <wulu@abv.bg>
2022-06-02 21:47:40 +0300

    created entities

---
+++ src/main/java/softuni/mobilele/model/entity/Offer.java
@@ -0,0 +1,10 @@
+package softuni.mobilele.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "offers")
+public class Offer extends BaseEntity{
+    //TODO fields...
+}
