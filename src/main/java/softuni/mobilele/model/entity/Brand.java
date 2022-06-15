849e5dd3e1c0344292f0024f3d2183649a7008b7
Lubo <wulu@abv.bg>
2022-06-15 22:46:27 +0100

    added lombok annotations

--- src/main/java/softuni/mobilele/model/entity/Brand.java
+++ src/main/java/softuni/mobilele/model/entity/Brand.java
@@ -1,21 +1,39 @@
 package softuni.mobilele.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "brands")
 public class Brand extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @Column
     private LocalDateTime created;
+
+    @Column
     private LocalDateTime modified;
 
+    @OneToMany(mappedBy = "brand",
+            targetEntity = Model.class,
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL)
     private List<Model> models;
 
 
-    @Column(unique = true, nullable = false)
+
     public String getName() {
         return name;
     }
@@ -42,10 +60,7 @@ public class Brand extends BaseEntity {
         this.modified = modified;
     }
 
-    @OneToMany(mappedBy = "brand",
-            targetEntity = Model.class,
-            fetch = FetchType.EAGER,
-            cascade = CascadeType.ALL)
+
     public List<Model> getModels() {
         return models;
     }
