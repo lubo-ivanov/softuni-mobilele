5e6f5658feaeccf00d5b78fe9cd134ef7a07da2b
Lubo <wulu@abv.bg>
2022-06-21 20:46:22 +0100

    added error annotations to Register Dto

--- src/main/java/softuni/mobilele/model/dto/UserRegistrationDto.java
+++ src/main/java/softuni/mobilele/model/dto/UserRegistrationDto.java
@@ -7,16 +7,33 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import softuni.mobilele.model.entity.UserRole;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserRegistrationDto {
-    private String firstName;
-    private String lastName;
-    private String username;
-    private String password;
-    private UserRole role;
 
-    //TODO construct the DTO
+    @NotEmpty(message = "First name should not be empty.")
+    @Size(min = 2, max = 20, message = "First name should be between 2 and 20 characters.")
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 20)
+    private String lastName;
+
+    @NotEmpty
+    @Size(min = 2, max = 20)
+    private String username;
+
+    @NotEmpty
+    @Size(min = 5)
+    private String password;
+
+    private String repeatPassword;
+
 }
