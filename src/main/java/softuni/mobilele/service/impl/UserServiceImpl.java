bb2ae42355175dad1b775a40ca04ff36ed5e1242
Lubo <wulu@abv.bg>
2022-06-22 20:26:17 +0100

    mapping

--- src/main/java/softuni/mobilele/service/impl/UserServiceImpl.java
+++ src/main/java/softuni/mobilele/service/impl/UserServiceImpl.java
@@ -1,20 +1,41 @@
 package softuni.mobilele.service.impl;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import softuni.mobilele.model.dto.UserRegistrationDto;
+import softuni.mobilele.model.entity.User;
 import softuni.mobilele.repository.UserRepository;
 import softuni.mobilele.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
+    private CurrentUser currentUser;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository,
+                           ModelMapper modelMapper, PasswordEncoder passwordEncoder, CurrentUser currentUser) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
+        this.currentUser = currentUser;
     }
 
     @Override
     public void registerAndLogin(UserRegistrationDto userRegistrationDto) {
-        //TODO implement method
+        User user = modelMapper.map(userRegistrationDto, User.class);
+        user.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
+        this.userRepository.save(user);
+        login(user);
+    }
+    private void login(User user) {
+        currentUser.
+                setLoggedIn(true).
+                setName(user.getFirstName() + " " + user.getLastName()).
+                setEmail(user.getUsername());
     }
 }
